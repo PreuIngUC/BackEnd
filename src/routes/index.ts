3a6bbfe328636b237f3c65d5b0864278ad21325b
Pablo Altamirano <p.j.altamirano.p@gmail.com>
2025-12-12 22:07:11 -0300

    fix: /api como prefix en koa para buen ruteo

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 import Router from '@koa/router'
 import healthRouter from './health.js'
 
-const router = new Router()
+const router = new Router({prefix: '/api'})
 
 router.use(healthRouter.routes(), healthRouter.allowedMethods())
 
