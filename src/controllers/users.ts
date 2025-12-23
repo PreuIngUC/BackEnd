1752017d1a7bd4175e289434cc50dce66bb036f4
Pablo Altamirano <p.j.altamirano.p@gmail.com>
2025-12-23 17:44:21 -0300

    student applications

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,33 +1,43 @@
 import type { BodyContext } from "../types/context.js";
-import type { StaffApplicationDtoType } from "../schemas/users/applications.js";
+import type { StaffApplicationDtoType, StudentApplicationDtoType } from "../schemas/users/applications.js";
 import DbApi from "../services/dbApi.js";
 
 const userService = DbApi.getInstance().user()
 
 export async function createStaffApplication( ctx: BodyContext<StaffApplicationDtoType> ) {
     const {user, staff} = ctx.request.body
-    
-    console.log('Creating staff application with data:', JSON.stringify({ user, staff }, null, 2))
-    
-    try {
-        const newApplication = await userService.create({
-            data: {
-                ...user,
-                auth0Id: null, // Explicitly set to null for applications without Auth0 yet
-                staffProfile: {
-                    create: {
-                        ...staff
-                    }
+    const newUser = userService.create({
+        data: {
+            ...user,
+            staffProfile: {
+                create: {
+                    ...staff
                 }
-            },
-            include: {
-                staffProfile: true
             }
-        })
-        ctx.status = 201
-        ctx.body = newApplication
-    } catch (error) {
-        console.error('Prisma error details:', error)
-        throw error
-    }
+        },
+        include: {
+            staffProfile: true
+        }
+    })
+    ctx.status = 201
+    ctx.body = newUser
+}
+
+export async function createStudentApplication( ctx: BodyContext<StudentApplicationDtoType> ) {
+    const {user, student} = ctx.request.body
+    const newUser = userService.create({
+        data: {
+            ...user,
+            studentProfile: {
+                create: {
+                    ...student
+                }
+            }
+        },
+        include: {
+            studentProfile: true
+        }
+    })
+    ctx.status = 201
+    ctx.body = newUser
 }
--- src/routes/public/users.ts
+++ src/routes/public/users.ts
@@ -1,14 +1,14 @@
 import Router from '@koa/router'
 import * as controller from '../../controllers/users.js'
 import validateThenHandle from '../../middlewares/validateThenHandle.js'
-import { StaffApplicationDto } from '../../schemas/users/applications.js'
+import { StaffApplicationDto, StudentApplicationDto } from '../../schemas/users/applications.js'
 
 const usersRouter = new Router()
 
 usersRouter.post('/staff/application',
     validateThenHandle({ body: StaffApplicationDto }, controller.createStaffApplication))
-// usersRouter.post('/student/application',
-//     validateThenHandle({ body: StudentApplicationDto })
-// )
+usersRouter.post('/student/application',
+    validateThenHandle({ body: StudentApplicationDto }, controller.createStudentApplication)
+)
 
 export default usersRouter
