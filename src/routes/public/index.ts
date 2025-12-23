bb40f2810dbb6d9e872e1ee13db5b6f371c19147
Pablo Altamirano <p.j.altamirano.p@gmail.com>
2025-12-23 16:04:24 -0300

    agregué res simple en / y /favicon.ico para que no den error las pruebas default de vercel

--- src/routes/public/index.ts
+++ src/routes/public/index.ts
@@ -4,6 +4,15 @@ import usersRouter from './users.js'
 
 const publicRouter = new Router()
 
+publicRouter.get("/", (ctx) => {
+  ctx.status = 200
+  ctx.body = "API running"
+})
+
+publicRouter.get("/favicon.ico", (ctx) => {
+  ctx.status = 204
+})
+
 publicRouter.use(healthRouter.routes(), healthRouter.allowedMethods())
 publicRouter.use(usersRouter.routes(), usersRouter.allowedMethods())
 
