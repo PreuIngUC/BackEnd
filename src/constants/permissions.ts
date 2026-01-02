7d6dfdbf79f5954c754d8b8e7df95f8adddb27ff
Pablo Altamirano <p.j.altamirano.p@gmail.com>
2026-01-02 19:30:58 -0300

    cambio en estados en la DB y nuevos permisos

--- src/constants/permissions.ts
+++ src/constants/permissions.ts
@@ -1,5 +1,8 @@
 enum Permissions {
   ReadServerHealth = 'read:server_health',
+  AcceptApplications = 'accept:applications',
+  CreateStudentUsers = 'create:student_users',
+  CreateStaffUsers = 'create:staff_users',
 }
 
 export default Permissions
