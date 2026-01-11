f8b550bf821b37a95052813c01c8f839c8771ebd
Pablo Altamirano <p.j.altamirano.p@gmail.com>
2026-01-10 22:22:14 -0300

    arreglo de ruta health

--- src/routes/public/health.ts
+++ src/routes/public/health.ts
@@ -1,16 +1,27 @@
 // import Permissions from '../constants/permissions.js'
 // import { UnauthorizedError } from '../utils/errors/auth0.js'
+import z from 'zod'
 import env from '../../config/env.js'
 import DocumentedRouter from '../../infrastructure/openapi/documentedRouter.js'
 
 const healthRouter = new DocumentedRouter('/api/public')
 
-healthRouter.get('/health', {}, async ctx => {
-  const environment = env.ITS_PREV ? 'PREVIEW' : env.ITS_PROD ? 'PRODUCTION' : 'DEVELOPMENT'
-  console.dir(ctx.state.user, { depth: null })
-  ctx.body = {
-    message: `El server funciona y está escuchando requests. El environment es ${environment}.`,
-  }
-})
+healthRouter.get(
+  '/health',
+  {},
+  async ctx => {
+    const environment = env.ITS_PREV ? 'PREVIEW' : env.ITS_PROD ? 'PRODUCTION' : 'DEVELOPMENT'
+    console.dir(ctx.state.user, { depth: null })
+    return {
+      message: `El server funciona y está escuchando requests. El environment es ${environment}.`,
+    }
+  },
+  {
+    status: 200,
+    schema: z.object({
+      message: z.string(),
+    }),
+  },
+)
 
 export default healthRouter
