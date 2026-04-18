812297723a52325da898e693b1a72df05a389d18
polnomas <p.j.altamirano.p@gmail.com>
2026-04-18 18:58:01 -0400

    fechas de inicion y final en enrolamientos, DTOs para creación de cursos y secciones

--- src/schemas/courses/input/courses.ts
+++ src/schemas/courses/input/courses.ts
@@ -1,19 +1,29 @@
-// import { z } from 'zod'
+import { z } from 'zod'
 import { CourseSchema, CourseEnrolmentSchema, SectionSchema, SectionEnrolmentSchema } from '../../../generated/zod/index.js'
 
-export const CourseInput = CourseSchema.omit({
-    id: true
-})
-
-export const CourseEnrolmentInput = CourseEnrolmentSchema.omit({
+export const CreateCourseDto = CourseSchema.omit({
     id: true,
+    createdAt: true
 })
 
-export const SectionInput = SectionSchema.omit({
-    id: true
+export const CreateCourseEnrolmentDto = CourseEnrolmentSchema.omit({
+    id: true,
+    createdAt: true
 })
 
-export const SectionEnrolmentInput = SectionEnrolmentSchema.omit({
-    id: true
+export const CreateSectionDto = SectionSchema.omit({
+    id: true,
+    createdAt: true
 })
+
+export const CreateSectionEnrolmentDto = SectionEnrolmentSchema.omit({
+    id: true,
+    createdAt: true
+})
+
+export type CreateCourseDtoType = z.infer<typeof CreateCourseDto>
+export type CreateCourseEnrolmentDtoType = z.infer<typeof CreateCourseEnrolmentDto>
+export type CreateSectionDtoType = z.infer<typeof CreateSectionDto>
+export type CreateSectionEnrolmentDtoType = z.infer<typeof CreateSectionEnrolmentDto>
+
 
